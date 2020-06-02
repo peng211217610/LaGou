2f49c958d86720d06c14353903293d118848843f
peng211217610 <peng211217610@163.com>
2020-06-02 22:43:07 +0800

    lesson1

---
+++ pythoncode/Lesson1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+#备注
+remark = ''''''
+
+#变量
+
+
+#引入相关包
+
+def func_with_parameter(a):
+    print(a)
+    return a
+
+
+def func_without_parameter():
+    print('nothing')
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    func_with_parameter(5)
+    func_without_parameter()
+    pass
